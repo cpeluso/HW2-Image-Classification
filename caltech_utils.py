7e06e02422d7f803f2bf8bde6c4023ccc96a534a
Carlo Peluso <53370003+cpeluso@users.noreply.github.com>
2020-05-23 15:43:15 +0200

    Create caltech_utils.py

---
+++ caltech_utils.py
@@ -0,0 +1,20 @@
+class CaltechUtils():
+    def __init__(self):
+      print("Initialize CaltechUtils")
+      pass
+
+    def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+    pass
+
+    def get_index(annotations, tuple):
+      index = 0
+      for annotation in annotations:
+        if annotation == tuple:
+          return index
+        else:
+          index += 1
+    pass
